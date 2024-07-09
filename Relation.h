9a4d79f6e671a8bc39d5a0eddd8c6f4987f76049
tribecon1 <bentl24@byu.edu>
2024-07-09 17:27:50 -0600

    2nd 'select' method for Relation, and Database class simply created

--- Relation.h
+++ Relation.h
@@ -27,13 +27,22 @@ public:
     }
 
     Relation select(int index, const string& value) const{
-        Relation result(name, column_headers);
+        Relation result(name, column_headers); //same specified value, same column
         for (const Tuple& tuple : tuples){
             if (tuple.at(index) == value){
                 result.addTuple(tuple);
             }
         }
+        return result;
+    }
 
+    Relation select(int index1, int index2){ //same unspecified value, diff. columns
+        Relation result(name, column_headers);
+        for (const Tuple& tuple : tuples){
+            if (tuple.at(index1) == tuple.at(index2)){
+                result.addTuple(tuple);
+            }
+        }
         return result;
     }
 
