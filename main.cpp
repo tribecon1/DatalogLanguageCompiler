37870fdc7fca7441dfc44903fad64bd4a2e871ac
tribecon1 <bentl24@byu.edu>
2024-07-19 14:16:21 -0600

    cleaning

--- main.cpp
+++ main.cpp
@@ -29,30 +29,6 @@ int main(int argc, char* argv[]) {
     Parser p = Parser(tokens);
     Datalog newDatalog = p.datalogParser();
 
-    /*vector<string> names = { "ID", "Name", "Major" };
-    Scheme scheme(names);
-    Relation relation("student", scheme);
-    vector<string> values[] = {
-            {"'42'", "'Ann'", "'CS'"},
-            {"'32'", "'Bob'", "'CS'"},
-            {"'64'", "'Ned'", "'EE'"},
-            {"'16'", "'Jim'", "'EE'"},
-    };
-
-    for (auto& value : values) {
-        Tuple tuple(value);
-        std::cout << tuple.toString(scheme) << std::endl;
-        relation.addTuple(tuple);
-    }
-
-    std::cout << "relation:" << std::endl;
-    std::cout << relation.toString();
-
-    Relation result = relation.select(2, "'CS'");
-
-    std::cout << "select Major='CS' result:" << std::endl;
-    std::cout << result.toString();*/
-
     Database newDatabase;
     Interpreter interpreter = Interpreter(newDatalog, newDatabase);
 
